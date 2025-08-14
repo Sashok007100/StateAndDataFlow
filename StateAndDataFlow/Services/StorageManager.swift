ac3b8056408bfc25640f50aea76b2a9bca5d1188
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-14 18:47:31 +0300

    Был создан StorageManager для работы с данными.

---
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StorageManager.swift
+//  StateAndDataFlow
+//
+//  Created by Alexandr Artemov (Mac Mini) on 14.08.2025.
+//
+
+import Foundation
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+}
